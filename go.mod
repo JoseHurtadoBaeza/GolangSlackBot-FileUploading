9f3bfa56f66fa7e9ffdf3dd007795e864ab1bed5
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2023-10-07 22:14:38 +0200

    Slack bot implemented and tested

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/JoseHurtadoBaeza/GolangSlackBot-FileUploading
 
 go 1.21.1
+
+require github.com/slack-go/slack v0.12.3
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))  // Specify this environment variable in the .bashrc file
+	channelArr := []string{os.Getenv("CHANNEL_ID")} // Specify this environment variable in the .bashrc file
+	fileArr := []string{"./HelloWorld.txt"}         // The file that will be sending out uploading basically
+
+	for i := 0; i < len(fileArr); i++ {
+
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+
+	}
+
+}
